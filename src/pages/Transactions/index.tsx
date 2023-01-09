2f7ae303957644e4bc014e7d6c74d7c77629294c
João Dias <joaoeduardodias123@gmail.com>
2023-01-09 10:50:26 -0300

    feat: created component table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,12 +1,38 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
-import { TransactionsContainer } from './styles'
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
-    <TransactionsContainer>
+    <div>
       <Header />
       <Summary />
-    </TransactionsContainer>
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$: 12.000,00</PriceHighLight>
+              </td>
+              <td>venda</td>
+              <td>13/04/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Hambúrguer</td>
+              <td>
+                <PriceHighLight variant="outcome">-R$: 2.000,00</PriceHighLight>
+              </td>
+              <td>alimentação</td>
+              <td>10/04/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
   )
 }
