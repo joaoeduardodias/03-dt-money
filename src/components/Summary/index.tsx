30332394dc36bee5aabea6e9ddfe4c67008167ba
João Dias <joaoeduardodias123@gmail.com>
2023-01-12 15:26:23 -0300

    feat: calculated summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,6 +7,21 @@ export function Summary() {
   const { transactions } = useContext(TransactionsContext)
   console.log(transactions)
 
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price
+        acc.total += transaction.price
+      } else {
+        acc.outcome += transaction.price
+        acc.total -= transaction.price
+      }
+
+      return acc
+    },
+    { income: 0, outcome: 0, total: 0 },
+  )
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -15,7 +30,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>R$: 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
@@ -23,7 +38,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>R$: 17.400,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
       <SummaryCard variant="green">
         <header>
@@ -31,7 +46,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>R$: 17.400,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
